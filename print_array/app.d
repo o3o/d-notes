48980dedbd2e795387e647eeb045e3f8fbf8ad11
Orfeo Da Via <orfeo.davia@microline.eu>
2016-04-07 14:28:30 +0200

    Aggiornato print_array

--- print_array/app.d
+++ print_array/app.d
@@ -7,7 +7,10 @@ void main(string[] args) {
    try {
       writefln("My items are %(%s, %).", [1,2,3]);
       writefln("My items are %(%s%).", [1,2,3]);
-      writefln("My items are %(%d %).", [1,2,3]);
+      writefln("My items are [%(%d %)]", [10,15,30]);
+      writefln("My items Hex [%(%x %)]", [10,15,30]);
+      writefln("My items Hex [%(0x%X %)]", [10,15,30]);
+
    } catch (Exception x) {
       writefln("oops: %s", x.msg);
    }
