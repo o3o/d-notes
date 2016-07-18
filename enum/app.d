1a4dbed474e39f4e47bf1ffbe4cbb82727d53c3a
Orfeo Da Via <orfeo.davia@microline.eu>
2016-07-18 15:14:31 +0200

    Migliorato enum

--- enum/app.d
+++ enum/app.d
@@ -1,16 +1,59 @@
 enum Suit { spades, hearts, diamonds, clubs }
-void main(string[] args) {
-   assert(enumToString(Suit.hearts) == "hearts");
-   assert(stringToEnum("diamonds") == Suit.diamonds);
-}
-import std.conv;
 
-string enumToString(Suit e) {
-   // cast(string)e; <- ERRORE
+void main(string[] args) {
+   // Enumerazione anonime
+   //----------------------------------------
+   // se non si dichara il tipo e' int
+   enum {top, bottom, left, right};
+   assert(typeid(top) == typeid(int));
+   // qui e' un ubyte
+   enum : ubyte { red, green, blue};
+   assert(typeid(red) == typeid(ubyte));
+
+   // Enumerazione anonime con un solo membro
+   //----------------------------------------
+   // Se l'Enumerazione ha un solo membro si possono omettere le graffe
+
+   enum {author0 = "Mike Parker"}
+   enum author1 = "Mike Parker";
+   enum string author2 = "Mike Parker";
+   assert(typeid(author0) == typeid(string));
+   assert(typeid(author1) == typeid(string));
+   assert(typeid(author2) == typeid(string));
+
+   // Conversioni da stringa a enum e viceversa
+   // ----------------------------------------
+   assert(suitToString(Suit.hearts) == "hearts");
+   assert(stringToSuit("diamonds") == Suit.diamonds);
+
+
+   // Conversioni da int a enum
+   // ----------------------------------------
+   enum Outcome : int {
+      unknown = 0,
+      na = 1,
+      pass = 2,
+      fail = 3,
+      stop = 4,
+      error = 5,
+   };
+   assert(cast(int)Outcome.fail == 3);
+   Outcome  o = cast(Outcome)5;
+   assert(o == Outcome.error);
+   // nel caso di enum senza valore il tipo e int e il valore inizia da zero
+   assert(cast(int)Suit.hearts == 1);
+
+   enum Rgb : ubyte { red = 1, green = 2, blue = 3 };
+   assert(cast(ubyte)Rgb.red == 1);
+}
+
+import std.conv;
+string suitToString(Suit e) {
+   //return cast(string)e; Error: cannot cast expression e of type Suit to string
    return to!(string)(e);
 }
 
-Suit stringToEnum(string s) {
-   // cast(string)e; <- ERRORE
+Suit stringToSuit(string s) {
+   //return cast(Suit)s; Error: cannot cast expression s of type string to Suit
    return to!(Suit)(s);
 }
--- stopwatch/app.d
+++ stopwatch/app.d
@@ -5,31 +5,38 @@ void main(string[] args) {
    StopWatch sw;
    // Avvio
    //----------------------------------------
-   // avviare piu' volte non genera problemi
-   // in realta' nel codice di phobos c'e un assert che verifica se start e' chaiato piu'
-   // volte, ma prob. phobos e' compilato coem release e l;assert eliminato
-
+   // avviare piu' volte non genera problemi.
+   // in realta' nel codice di phobos c'e' un assert che verifica se start e' chiamato piu'
+   // volte, ma prob. phobos e' compilato coem release e l'assert eliminato
 
    sw.start();
    sw.start();
    sw.start();
    auto t1 = sw.peek();
    sw.stop();
-   auto t2 = sw.peek();
-   auto t3 = sw.peek();
+   TickDuration t2 = sw.peek();
+   TickDuration t3 = sw.peek();
    assert(t1 <= t2);
    assert(t2 == t3);
 
    writefln("%s sec, %s msec %s", t1.seconds, t1.msecs, typeid(t1.seconds));
+   // Peek ritorna TickDuration che ha le funzioni seconds e msecs che ritornano long
+   long sec = sw.peek.seconds;
+   long msecs = sw.peek.msecs;
+   int max = 3;
+   // long si puo' confrontare con int
+   if (sec > max) {
+   }
 
 
    // Reset
    //----------------------------------------
    // Per eseguire il reset e' necessario lo stop
-   // Se si esegue il reset senza stop l'asserzione fallisce
+   // Se si esegue il reset senza stop l'asserzione sotto fallisce
    sw.start();
    sw.stop();
    sw.reset();
+   // senza stop queste asserzioni falliscono:
    assert(sw.peek().to!("seconds", real)() == 0);
    assert(sw.peek().msecs == 0);
 
