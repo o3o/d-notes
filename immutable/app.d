48added3f8e4e9afd97a38f6ba6e77e6effc8f8f
Orfeo Da Vià <orfeo.davia@microline.eu>
2017-05-11 08:29:08 +0200

    Modifiche a immutable

--- immutable/app.d
+++ immutable/app.d
@@ -1,30 +1,31 @@
+import std.stdio;
 struct A {
    void fun() {}
    void gun() immutable {}
 }
+
 struct B {
    int x;
    int y;
-
 }
 
 void main(string[] args) {
 
-   /*
-    * Perche' devo metterci new?
-    */
-   auto a1 = new A();
-   auto a2 = new immutable(A);
+   A a1 = A();
    a1.fun();
-
    //a1.gun(); //immutable method app.A.gun is not callable using a mutable object
+
+   auto a2 = new immutable(A);
    //a2.fun(); // Error: mutable method app.A.fun is not callable using a immutable object
    a2.gun();
 
    /*
-    * e qui no?
+    * Cosa accade se si dichiara
+    * B b = new B();
+    * Error: cannot implicitly convert expression (new B) of type B* to B
     */
-   B b = B();
-
-   // B c = new B(); // Error: cannot implicitly convert expression (new B(0, 0)) of type B* to B
+   B* b = new B();
+   B c = *b;
+   (*b).x = 42;
+   assert(c.x == 0);
 }
